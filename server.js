5b7d8cea12faac5c351e1394b32279e47b4f77bf
Abdulaziz6665 <mr.yunusobod@gmail.com>
2021-06-13 23:48:02 +0500

    asas

--- server.js
+++ server.js
@@ -18,15 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     res.sendFile(path.join(__dirname, '../client-side', 'build', 'index.html'))
 // })
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-      
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
 
 const SQL = `
     select
@@ -116,6 +107,16 @@ app.post('/contacts', async (req, res) => {
             console.log(e.message)
      }
 })
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 
 
 app.listen(PORT, () => console.log('server is running ' + PORT))
